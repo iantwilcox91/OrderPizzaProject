8103634165c3d37a3fbae08740ca87f9844e02e8
iantwilcox91 <iantwilcox91@>
2016-08-26 14:15:07 -0700

    all information is also shown with result cost

--- js/scripts.js
+++ js/scripts.js
@@ -9,29 +9,30 @@ Pizza.prototype.checkoutCost = function () {
 };
 
 function PostPizza(){
+  var selectedSize = $('input[name=size]:checked', '#sizeOfPizza').val();
+  var selectedToppings = $( ":checkbox:checked" ).map(function() { return this.value; }).get().join().split(",");
+  var newPizza = new Pizza(selectedSize, selectedToppings);
 
-
+  if (newPizza.size === "1"){
+    var SizeString = "small";
+    $(".chosenSize").text(SizeString);
+  }else if(newPizza.size === "2"){
+    var SizeString = "medium";
+    $(".chosenSize").text(SizeString);
+  }else if(newPizza.size === "3"){
+    var SizeString = "large";
+    $(".chosenSize").text(SizeString);
+  }
+  $(".chosenToppings").text(newPizza.topping);
+  $(".willCost").text("$" + newPizza.checkoutCost() +".00");
 }
-
-
-
-
 
 
 // user interface logic
 $(document).ready(function(){
 
   $("#goButton").click(function() {
-    var selectedSize = $('input[name=size]:checked', '#sizeOfPizza').val();
-    var selectedToppings = $( ":checkbox:checked" ).map(function() { return this.value; }).get().join().split(",");
-    var newPizza = new Pizza(selectedSize, selectedToppings);
-    console.log(newPizza);
-    console.log(newPizza.size);
-    $(".chosenSize").text(newPizza.size);
-    console.log(newPizza.topping);
-    $(".chosenToppings").text(newPizza.topping);
-    console.log(newPizza.checkoutCost());
-    $(".willCost").text("$" + newPizza.checkoutCost() +".00")
+    PostPizza();
   });
 
 });
